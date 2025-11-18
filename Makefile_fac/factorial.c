b6e5a3004d2f075a9dbaf19c51d24ca63d824faa
srinivas-ece <srinivasvinnakota799@gmail.com>
2025-11-18 22:15:57 +0530

    Delete Makefile_fac directory

--- Makefile_fac/factorial.c
+++
@@ -1,8 +0,0 @@
-#include "factorial.h"
-
-int fact(int x){
-    if (x == 0 || x == 1)
-        return 1;
-    return x * fact(x - 1);
-}
-
--- Makefile_fac/factorial.h
+++
@@ -1,7 +0,0 @@
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
-
-int fact(int x);
-
-#endif
-
